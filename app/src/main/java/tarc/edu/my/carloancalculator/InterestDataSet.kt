61f6dff9c1e51d06fcd6dbd905acd3d9dafe5000
okito123 <chanowen123@gmail.com>
2022-07-28 09:54:01 +0800

    commit

---
+++ app/src/main/java/tarc/edu/my/carloancalculator/InterestDataSet.kt
@@ -0,0 +1,6 @@
+package tarc.edu.my.carloancalculator
+//TODO 2: Define a data class
+class InterestDataSet(var bank: String = "",
+                      var interest: String = "") {
+
+}
---
+++ app/src/main/java/tarc/edu/my/carloancalculator/SecondFragment.kt
@@ -0,0 +1,52 @@
+package tarc.edu.my.carloancalculator
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import tarc.edu.my.carloancalculator.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        //_binding = FragmentSecondBinding.inflate(inflater, container, false)
+        //TODO 4:Initialise Data Binding Class instead view binding
+        _binding=DataBindingUtil.inflate(inflater,R.layout.fragment_second,container,false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //TODO 5: Create an instance of data set
+        val interestDataSet = InterestDataSet("CIMB\nMaybank\nPublicBank\nRHB","2.95%(p.a)\n2.99%(p.a)\n3.05%(p.a)\n3.10&(p.a)")
+
+        //TODO 6:connect data set to layout
+        binding.interestDataSet = interestDataSet
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
